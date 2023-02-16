0400b8ae7af911db99bb7e33e6d529b2151592f5
Mattia Collalti <collalti.mattia@gmail.com>
2023-02-16 11:02:04 +0100

    implement a first basic solution

--- src/app/temperatureConverter/temperatureConverter.component.ts
+++ src/app/temperatureConverter/temperatureConverter.component.ts
@@ -1,4 +1,5 @@
-import {Component, Input, OnInit} from '@angular/core';
+import { DecimalPipe } from '@angular/common';
+import { Component, Input, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'temperature-converter',
@@ -6,11 +7,29 @@ import {Component, Input, OnInit} from '@angular/core';
   styleUrls: ['./temperatureConverter.component.scss']
 })
 
-export class TemperatureConverter implements OnInit {
+export class TemperatureConverter {
 
-  ngOnInit() {
-    // C = (F − 32) × 5/9
-    // F = C*9/5 + 32
+
+  private _celsius: number;
+  public get celsius(): number {
+    return this._celsius;
   }
+  public set celsius(v: number) {
+    if (this._celsius == v) return;
+    this._celsius = v;
+    this._farenheit = Number(this.decimalPipe.transform(this.celsius * 9 / 5 + 32, '1.1-1'));
+  }
+
+  private _farenheit: number;
+  public get farenheit(): number {
+    return this._farenheit;
+  }
+  public set farenheit(v: number) {
+    if (this._farenheit == v) return;
+    this._farenheit = v;
+    this._celsius = Number(this.decimalPipe.transform((this.farenheit - 32) * 5 / 9, '1.1-1'));
+  }
+
+  constructor(private decimalPipe: DecimalPipe) { }
 
 }
